2266bb493d3afc188be0466d6a95aab80f97de79
Damian Icely <damianicely@gmail.com>
2019-11-26 15:05:00 +0000

    completed version

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+// return a random number 0:9
+const generateTarget = () => Math.floor(Math.random()*10);
+// return the absolute distance between target number and guess
+const getAbsoluteDistance = (target, guess) => Math.abs(target-guess);
+
+// function to compare guesses
+//returns true if human wins or ties with computer
+// returns false if computer wins
+const compareGuesses = (userGuess, computerGuess, target) => {
+    if ((userGuess > 9)||(userGuess < 0)){
+        alert('You must enter a number between 0 and 9!')
+    }
+    let howCloseUser = getAbsoluteDistance(target, userGuess);
+    let howCloseComputer = getAbsoluteDistance(target, computerGuess);
+    if ((howCloseUser === howCloseComputer)||(howCloseUser < howCloseComputer)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+// function to update the scores based on winner
+const updateScore = winner => (winner === 'computer') ? humanScore += 1 : computerScore += 1; 
+// Function to advance the round, updating the round number
+const advanceRound = () => currentRoundNumber += 1;
